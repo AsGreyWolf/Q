0b18a5d7a36104650d372cc99827d24be8b213fe
Spokxxl <Spokxxl@yandex.ru>
2012-10-16 18:16:17 +0400

    текст

---
+++ src/engine/client/textrender.h
@@ -0,0 +1,14 @@
+#ifndef _TEXTRENDER_H_
+#define _TEXTRENDER_H_
+
+#include <SDL.h>
+#include "gl.h"
+#include "glu.h"
+ 
+class TextRender
+{
+    public:
+       GLuint* loadimage(char* filename,GLuint* image);
+       graphics();
+};
+#endif
