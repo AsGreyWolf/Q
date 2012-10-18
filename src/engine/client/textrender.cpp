f9a7253227342d23a9de4d1f07ce9ce7915cb4c2
A0 <asschool@yandex.ru>
2012-10-18 20:34:08 +0600

    image text

--- src/engine/client/textrender.cpp
+++ src/engine/client/textrender.cpp
@@ -1,6 +1,7 @@
 #ifndef _TEXTRENDER_CPP_
 #define _TEXTRENDER_CPP_
 #include "textrender.h"
+#include "SDL_image.h"
 int TextRender::Init()
 {
 	TTF_Init();
@@ -18,5 +19,48 @@ void TextRender::print_ttf(SDL_Surface *sDest, char* message, char* font, int si
 void TextRender::TextRender_Exit()
 {
 	atexit(TTF_Quit);
+}
+SDL_Surface* TextRender::LoadFont(char* BitmapFont){
+
+  SDL_Surface* temp;
+  
+  temp = IMG_Load(BitmapFont);
+  if(temp == NULL){
+  	printf("Can't load image %s\n",BitmapFont);
+	return NULL;
+  }
+  
+  SDL_SetColorKey(temp,SDL_SRCCOLORKEY,SDL_MapRGB(temp->format,255,255,255));
+  
+  return temp;
+
+}
+
+/* ------------------------------------------------------ */
+void TextRender::DrawText(SDL_Surface* sFont, int x, int y, char * str,SDL_Surface* screen){
+
+  int i;
+  SDL_Rect src, dest;
+  
+  for (i = 0; i < strlen(str); i++)
+    {
+      if (str[i] >= 'A' && str[i] <= 'Z')
+	{
+	  src.x = (str[i] - 'A') * 32;
+	  src.y = 0;
+	  src.w = 32;
+	  src.h = 32;
+	  
+	  dest.x = x + (i * 32);
+	  dest.y = y;
+	  dest.w = 32;
+	  dest.h = 32;
+	  
+	  SDL_BlitSurface(sFont, &src,
+			  screen, &dest);
+	}
+    }
+  SDL_Flip(screen);
+  
 }
 #endif
--- src/engine/client/textrender.h
+++ src/engine/client/textrender.h
@@ -11,5 +11,7 @@ class TextRender
 	int Init();
 	void TextRender_Exit();
 	void print_ttf(SDL_Surface *sDest, char* message, char* font, int size, SDL_Color color, SDL_Rect dest);
+SDL_Surface* LoadFont(char* BitmapFont);
+void DrawText(SDL_Surface* sFont, int x, int y, char * str,SDL_Surface* screen);
 };
 #endif
